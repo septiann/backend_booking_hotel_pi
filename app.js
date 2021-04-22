41d68eb8fb0cc0ff9fb20a2fb90da12856e56d13
Septian Nugraha <septiann@outlook.com>
2021-04-22 14:58:14 +0700

    handling cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+const cors = require('cors')
 
 // import mongoose
 const mongoose = require('mongoose')
@@ -46,6 +47,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')))
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
